1ea6c49ae10b5c04d6128bc56a785f190f7b8a21
Mindi <Mindii@users.noreply.github.com>
2016-12-30 18:20:55 +0200

    Add files via upload

---
+++ steam.py
@@ -0,0 +1,25 @@
+import os, sys
+
+## Steam Settings
+SteamDir = 'C:\Program Files (x86)\Steam\Steam.exe'
+
+## Control steam ##
+def SteamCMD(CMD='False'):
+    if CMD == 'False':
+        return 'No command given'
+        sys.exit(1)
+    elif CMD == 'away':
+        SteamCmd = 'friends/status/away'
+    elif CMD == 'busy':
+        SteamCmd = 'friends/status/busy'
+    elif CMD == 'offline':
+        SteamCmd = 'friends/status/offline'
+    elif CMD == 'online':
+        SteamCmd = 'friends/status/online'
+    else:
+        return 'no valid command, look commands over here: https://developer.valvesoftware.com/wiki/Steam_browser_protocol'
+        sys.exit(1)
+    SteamRun = '"%s" steam://%s' %(SteamDir, SteamCmd)
+    os.popen(SteamRun)
+		
+print(SteamCMD(sys.argv[1]))
